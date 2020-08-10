fecbee42dc4f78e1487916773c295232978f9f42
TriNguyenGit <nguyenvantri51480153@gmail.com>
2020-08-10 19:11:31 +0700

    Tính đóng gói

--- app/src/main/java/com/example/oop13072020/Animal.java
+++ app/src/main/java/com/example/oop13072020/Animal.java
@@ -5,9 +5,9 @@ import android.util.Log;
 import android.widget.Switch;
 
 public class Animal {
-    String category;
-    int height;
-    int weight;
+   private String category;
+   private int height;
+   private int weight;
     public void eat(Food food){
         switch(food){
             case MEAT:
@@ -18,5 +18,29 @@ public class Animal {
                 break;
         }
         Log.d("BBB","");
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }
